e1e671e7f02e97137e8aff51f11ece2795bb40fc
SantiBarre22 <santibarre022@gmial.com>
2021-08-31 15:36:51 -0300

    1.3 Mejore velocimetro poniendo 4 himanes

--- Master/Velocimetro.ino
+++ Master/Velocimetro.ino
@@ -1,9 +1,11 @@
-#define sensor_velocidad  2            //PIN DE VELOCIMETRO (con interrupciones)
-#define perimetro  0.8    //PERIMETRO DE LA RUEDA en m
+#define sensor_velocidad  3            //PIN DE VELOCIMETRO (con interrupciones)
+#define perimetro  2.2    //PERIMETRO DE LA RUEDA en m
 #define interval 1000     //Tiempo en dar un valor
-byte velocidadms = 0;       //Velocidad redondeada
-byte pulsos = 0; 
+
+float velocidadms = 0;       //Velocidad redondeada
+unsigned int pulsos = 0; 
 byte velocidad = 0;
+
 unsigned long previousMillis = 0; //Inicializa los millis
 
 void inicia_velocimetro(){
@@ -20,9 +22,9 @@ int Velocimetro() {
     if (currentMillis - previousMillis >= interval) {
     
         previousMillis = currentMillis;               //Funcional para la repeticion
-        velocidadms = (pulsos * perimetro) / (interval/1000);     // Multiplico cantidad de vueltas por el perimero
+        velocidadms = ((pulsos/4) * perimetro) / (interval/1000);     // Multiplico cantidad de vueltas por el perimero
                                                        // Y dibido por la cantida de tiempo que le tomo hacer esa distacia
-        velocidad = round(velocidadms * 3.6);
+        velocidad = round(velocidadms * 3.600);
         pulsos = 0;                              //Reset de los pulsos
         return velocidad;             //Me da la velocidad
     }
